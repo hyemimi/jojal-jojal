e6f9dfc4839a74f2d4f8dcd9b4be0ad2c2cdebb2
hyemimi <hyemi7375@gmail.com>
2025-02-20 22:58:29 +0900

    헤더-프로필 페이지 퍼블리싱

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,21 @@
+import { navigateTo } from "../router.js";
+
 export default function Header () {
     const header = document.getElementById("header");
     
     header.innerHTML = `
     <div class="header-div">
         <h1>아무 말 대잔치</h1>
-        <div class="profile"></div>
+        <div id="profile"></div>
     </div>
   
     `;
+    const Profile = document.getElementById("profile")
+    const ProfileImage = document.createElement("img")
+    ProfileImage.src = './src/assets/character.jpg'
+    ProfileImage.classList.add("profile-image")
+    Profile.appendChild(ProfileImage)
+
+    ProfileImage.addEventListener("click", ()=> navigateTo('/profile'))
 
 }
