e3427b793dcd2e8383a7733423f084318f98b3b6
hyemimi <hyemi7375@gmail.com>
2025-03-20 17:10:03 +0900

    feat: 로그인 api 연결

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,6 +1,7 @@
 
 import Button from "../../components/Button.js";
 import { navigateTo } from "../../router.js";
+import Config from "../../config.js";
 
 export default function LoginPage() {
     const app = document.getElementById("app");
@@ -16,6 +17,7 @@ export default function LoginPage() {
             <input type="password" placeholder="비밀번호를 입력하세요" id = "password"></input>
             <span class="error-message" id="password-error"></span>
         </form>
+        <span class="error-message" id="login-error"></span>
         <p>
         아직 회원이 아니신가요? 
         <a href="/register" id="register-link">회원가입</a> 
@@ -28,7 +30,7 @@ export default function LoginPage() {
     const passwordInput = document.getElementById("password");
     const emailError = document.getElementById("email-error");
     const passwordError = document.getElementById("password-error");
-    //const loginError = document.getElementById("login-error");
+    const loginError = document.getElementById("login-error");
 
     const submitButton = Button({
         type: "submit",
@@ -48,7 +50,7 @@ export default function LoginPage() {
         // 기존 에러 메시지 초기화
         emailError.textContent = "";
         passwordError.textContent = "";
-        //loginError.textContent = "";
+        loginError.textContent = "";
       
         const email = emailInput.value.trim();
         const password = passwordInput.value.trim();
@@ -73,8 +75,8 @@ export default function LoginPage() {
 
         // 로그인 요청
         try {
-            navigateTo("/boards");
-           /*  const response = await fetch("https://your-api.com/login", {
+                
+             const response = await fetch(`${Config.API_URL}/auth`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -82,13 +84,12 @@ export default function LoginPage() {
                 body: JSON.stringify({ email, password }),
             });
 
-            const result = await response.json();
-
             if (response.ok) {
+                // 로그인 성공공
                 navigateTo("/boards");
-            } else {
+            } else if (response.status == 401) {
                 loginError.textContent = "이메일과 비밀번호를 확인해주세요.";
-            } */
+            } 
         } catch (error) {
             console.error("로그인 요청 중 오류 발생:", error);
             loginError.textContent = "서버 오류가 발생했습니다.";
